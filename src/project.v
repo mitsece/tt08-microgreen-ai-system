ba014ce53627b9b3d631593e0328714eef628ed8
mitsece <166103244+mitsece@users.noreply.github.com>
2026-02-19 01:08:31 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 // ============================================================
 
 `default_nettype none
-`timescale 1ns / 1ps
+
 
 module tt_um_precision_farming (
     input  wire [7:0] ui_in,    // Dedicated inputs
